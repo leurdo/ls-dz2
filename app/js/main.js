a6915583a6c5d12e5db3cf862cf393290d13a7a4
Katya Leurdo <edzakharova@yandex.ru>
2016-02-26 09:06:08 +0300

    Финиш

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-//Price select
+
 (function() {
 
     publicMethod();
@@ -6,19 +6,28 @@
     attachEvents();
 
     function init() {
+      $(function(){
+
         priceSelect();
         initSelect2();
+        columnize();
+
+        });
     };
 
     function attachEvents() {
         $('.slider__item').on('click', onImageSlider);
         $('.view__item').on('click', onChangeView);
+        $('.fb-body__link').on('click', onClearInputs);
+        $('.fb-header').on('click', onCloseBlocks);
     };
 
     function priceSelect() {
         var 
           min = parseInt($('#amount1').attr('data-min')),
-          max = parseInt($('#amount2').attr('data-max'));
+          max = parseInt($('#amount2').attr('data-max')),
+          val1 = min,
+          val2 = max;
 
         $('#slider-range').slider({
           range: true,
@@ -33,10 +42,34 @@
         $('#amount1').val( $('#slider-range').slider('values', 0 ) );
         $('#amount2').val( $('#slider-range').slider('values', 1 ) );
 
-        $('#amount1').keyup(function() {
+        $('#amount1').change(function() {
+            val1 = $(this).val();
+            val2 = $('#amount2').val();
+            
+            if (parseInt(val1) > parseInt(val2)) {
+              val1 = val2;
+            }
+            if (parseInt(val1) < min || val1 == '') {
+              val1 = min;
+            }
+            
+            $(this).val(val1);
+
             $('#slider-range').slider('values', 0, $(this).val() );
         });
-        $('#amount2').keyup(function() {
+
+        $('#amount2').change(function() {
+            val1 = $('#amount1').val();
+            val2 = $(this).val();
+
+            if (parseInt(val2) < parseInt(val1)) {
+              val2 = val1;
+            }
+            if (parseInt(val2) > max || val2 == '') {
+              val2 = max;
+            }
+            $(this).val(val2);
+
             $('#slider-range').slider('values', 1, $(this).val() );
         });
     };
@@ -83,7 +116,29 @@
           .removeClass(currentClass)
           .addClass(viewClass);
       }
-    }
+    };
+
+    function onClearInputs(e) {
+      e.preventDefault();
+      var
+      $this = $(this);
+
+      $this.closest('.fb-body').find('input').removeAttr('checked');
+    };
+
+    function onCloseBlocks(e) {
+      e.preventDefault();
+      var
+      $this = $(this);
+
+      $this.closest('.formblock').find('.fb-body').slideToggle();
+      $this.toggleClass('fb-header_closed')
+    };
+
+    function columnize() {
+      $('.information__p').addClass('dontsplit');
+      $('.information__text').columnize({ columns: 2, lastNeverTallest: true });
+    };
 
     function publicMethod() {};
 
